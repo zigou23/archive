e2478be65d35f6786e315b302fdd987a63770e7d
zigouisim <40587032+zigou23@users.noreply.github.com>
2020-08-29 17:03:57 +0800

    Create 403.js

---
+++ CloudFlare/403.js
@@ -0,0 +1,7 @@
+addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  // host地址
+  url.hostname = 'aindex.herokuapp.com';
+  const request = new Request(url, event.request);
+  event.respondWith(fetch(request));
+});
