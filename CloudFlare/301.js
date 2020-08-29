c7003029b37998db9a794e28027cf8d42f4f25f1
zigouisim <40587032+zigou23@users.noreply.github.com>
2020-08-29 17:06:28 +0800

    Update and rename 403.js to 301.js

--- CloudFlare/301.js
+++ CloudFlare/301.js
@@ -1,3 +1,4 @@
+//1
 addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   // host地址
@@ -5,3 +6,5 @@ addEventListener('fetch', event => {
   const request = new Request(url, event.request);
   event.respondWith(fetch(request));
 });
+
+
